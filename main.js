d329f183c495100de19acb84f7e66b40058e47c9
irinka <i.bakhareva@mail.ru>
2016-06-05 14:42:13 +0400

    bug fixes

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 window.table_item = [];
-window.price = 'Price';
 
 function initItem() {
     if (isCorrect()) {
-        var item = {
-            name: $('#name').val(),
-            count: Number($('#count').val()),
-            price: Number($('#price').val())
-        };
-
+        if ($('#price').val().substr(0, 1) == '$') {
+            var item = {
+                name: $('#name').val(),
+                count: Number($('#count').val()),
+                price: Number(reverseConvert($('#price').val()))
+            };
+        } else {
+            var item = {
+                name: $('#name').val(),
+                count: Number($('#count').val()),
+                price: Number($('#price').val())
+            };
+        }
         table_item.push(item);
         drowNewRow(table_item.length - 1);
         addEventDelete(table_item.length - 1);
@@ -22,16 +28,16 @@ function isCorrect() {
     var flag = false;
     if ($('#name').val() == '' || pattern.test(($('#name').val()))) {
         $('#error').text('Поле Name не может быть пустым!');
-        $('#name').css('border-color', 'rgb(176,0,0)');
+        $('#nameFormGroup').addClass('has-error');
     }
     else if ($('#name').val().length >= 15) {
         $('#error').text('Максимальная длина 15 символов!');
-        $('#name').css('border-color', 'rgb(176,0,0)');
+        $('#nameFormGroup').addClass('has-error');
     }
     else {
         flag = true;
         $('#error').text('');
-        $('#name').css('border-color', 'rgb(238,238,238)');
+        $('#nameFormGroup').removeClass('has-error');
     }
 
     return flag;
@@ -43,11 +49,12 @@ function drowNewRow(num) {
     if (table_item[num] === undefined) {
         return;
     }
-    if (table_item[num].name.toLowerCase().indexOf($('#filter').val().toLowerCase())>=0) {
+    if (table_item[num].name.toLowerCase().indexOf($('#filter').val().toLowerCase()) >= 0) {
         $('tbody').append(
             "<tr id='tr" + num + "''><td>" + table_item[num].name + "<span class='pull-right'>" + table_item[num].count + "</span></td><td>"
-            + convert(table_item[num].price) + "</td><td><input id='edit" + num + "' type='submit' value='Edit'><input id='delete"
-            + num + "'type='submit' value='Delete'></td></tr>"
+            + convert(table_item[num].price) + "</td><td>" +
+            "<input id='edit" + num + "' type='submit' value='Edit' class='btn btn-success'>" +
+            "<input id='delete" + num + "'type='submit' value='Delete' class='btn btn-danger'></td></tr>"
         );
     }
 }
@@ -67,9 +74,8 @@ function addEventEdit(num) {
         $('#add').val('Update');
         $('#name').val('' + table_item[num].name);
         $('#count').val('' + table_item[num].count);
-        $('#price').val('' + table_item[num].price);
+        $('#price').val(convert(table_item[num].price));
         isCorrect();
-
         if ($('#add').val() == "Update") {
             $('#add').unbind('click');
             $('#add').click(function () {
@@ -77,6 +83,10 @@ function addEventEdit(num) {
                     delete table_item[num];
                     $('#tr' + num + '').remove();
                     initItem();
+                    $('#name').val('');
+                    $('#count').val('');
+                    $('#price').val('');
+                    $('#add').unbind('click');
                 }
             });
         }
@@ -106,29 +116,29 @@ function redraw() {
 }
 
 //Сортировка обратная
-function eventReverseSort(id,compare) {
+function eventReverseSort(id, compare) {
     $(id).unbind('click');
     $(id).click(function () {
         table_item.sort(compare);
         table_item.reverse();
         redraw();
         $(id).css('transform', 'none');
-        eventSort(id,compare);
+        eventSort(id, compare);
     });
 }
 
 //Сортировка прямая
-function eventSort(id,compare) {
+function eventSort(id, compare) {
     $(id).unbind('click');
     $(id).click(function () {
         table_item.sort(compare);
         redraw();
         $(id).css('transform', 'rotate(180deg)');
-        eventReverseSort(id,compare);
+        eventReverseSort(id, compare);
     });
 }
 
-function eventFilter(){
+function eventFilter() {
     $('#fltr').click(function () {
         //if ($('filter').val()==''){
         //    return;
@@ -140,6 +150,16 @@ function eventFilter(){
 function convert(price) {
     var money = '$' + price.toString().split(/(?=(?:\d{3})+$)/);
     return money;
+}
+
+function reverseConvert(money) {
+    if (money.substr(0, 1) == '$' && money !== undefined) {
+        var price = money.substr(1);
+        return price.replace(/\D/g, '');
+    } else {
+        return '';
+    }
+
 }
 
 //событие для add
@@ -164,14 +184,18 @@ $('#addnew').click(function () {
 
 //событие для конвертации
 $('#price').blur(function () {
-    window.price = $('#price').val();
     $('#price').val(convert(Number($('#price').val())));
 });
 
 $('#price').focus(function () {
-    $('#price').val(window.price);
-})
+    $('#price').val(reverseConvert($('#price').val()));
+});
 
-eventSort('#srt1',compareItemName);
-eventSort('#srt2',compareItemPrice);
+$('#count').keyup(function () {
+    var val = $('#count').val().replace(/\D/g, '');
+    $('#count').val(val);
+});
+
+eventSort('#srt1', compareItemName);
+eventSort('#srt2', compareItemPrice);
 eventFilter();
